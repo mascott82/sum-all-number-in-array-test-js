cd2eb622539dc0e7064601a79f552019f11d2fd7
mascott82 <m.a.scott@live.com>
2023-11-16 15:42:37 -0600

    Implementing the function sumItems().

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function sumItems(array) {
+const sumItems = function(array) {
+
+  let result = 0;
   // Sum all the numbers in the array
-}
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      result += sumItems(element);
+    } else {
+      result += element;
+    }
+  });
+
+  return result;
+};
 
 module.exports = sumItems;
